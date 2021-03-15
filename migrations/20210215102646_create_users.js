50d82e3b6fb4152b6ddae7ec25ec014f6b800aa0
Preya <preya16@hotmail.com>
2021-03-15 13:05:21 +0800

    Add encrypted_password data field in create_users

--- migrations/20210215102646_create_users.js
+++ migrations/20210215102646_create_users.js
@@ -5,6 +5,7 @@ exports.up = function(knex) {
         table.string('name');
         table.string('email');
         table.string('password');
+        table.string('encrypted_password');
         table.string('role');
         table.timestamps();
       });
